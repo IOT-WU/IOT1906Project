052b3f529937597b2d923881186593017441913f
WuJunHaoQ <99776577+WuJunHaoQ@users.noreply.github.com>
2022-02-25 09:28:06 +0800

    Update BaseModels.cs

--- DomainDTO/EFModels/BaseModels.cs
+++ DomainDTO/EFModels/BaseModels.cs
@@ -1,4 +1,9 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace DomainDTO.EFModels
 {
