14a400ccd2aacdec850a009045cb830fdeb6fd12
tokzhang <2654825153@qq.com>
2022-02-25 11:53:18 +0800

    员工录用--EFModels新建两个表--张贺

--- MyDB/MyDBContext.cs
+++ MyDB/MyDBContext.cs
@@ -10,5 +10,7 @@ namespace MyDB
         public MyDBContext(DbContextOptions<MyDBContext> options) : base(options) { }
         public virtual DbSet<Leave> Leave { get; set; } //请假流程申请表
         public virtual DbSet<BPMSysUsers> BPMSysUsers { get; set; } //用户信息表
+        public virtual DbSet<Employed> Employed { get; set; } //员工录入核定表
+        public virtual DbSet<Relative> Relative { get; set; } //员工亲属信息表
     }
 }
