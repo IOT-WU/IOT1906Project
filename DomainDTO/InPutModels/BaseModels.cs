2fa9fc8cf57a1e1208c606941af1e805fe062f79
guoyankai <2408835196@qq.com>
2022-02-25 11:26:39 +0800

    修改登录模式为BPM用户信息登录

---
+++ DomainDTO/InPutModels/BaseModels.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace bpmdemoapi.models
+{
+    [NotMapped]
+    public class BaseModels
+    {
+        public string Action { get; set; }
+        public string BPMUser { get; set; }
+        public string BPMUserPass { get; set; }
+        public string FullName { get; set; }
+        public string ProcessName { get; set; }
+    }
+}
