16372f0eec24aa9c52e3669d3baabf305e806203
俊杰 崔 <cjj34377332601@outlook.com>
2022-03-01 14:28:51 +0800

    人力资源-崔俊杰

---
+++ DomainDTO/EFModels/MInsurance.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainDTO.EFModels
+{
+   public class MInsurance
+    {
+        [Key]
+        public int car_AId { get; set; }
+        public int TaskId { get; set; }
+        /// <summary>
+        /// 经办人
+        /// </summary>
+        public string car_Handler { get; set; }
+        /// <summary>
+        /// 制表时间
+        /// </summary>
+        public DateTime car_Tabulation { get; set; }
+        /// <summary>
+        /// 车型
+        /// </summary>
+        public string car_Model { get; set; }
+        /// <summary>
+        /// 车牌号
+        /// </summary>
+        public string car_number { get; set; }
+        /// <summary>
+        /// 购买日期
+        /// </summary>
+        public DateTime car_purchase { get; set; }
+        /// <summary>
+        /// 座位数
+        /// </summary>
+        public int car_seat { get; set; }
+        /// <summary>
+        /// 保险金额
+        /// </summary>
+        public decimal car_money { get; set; }
+        /// <summary>
+        /// 乘客险
+        /// </summary>
+        public string car_passenger { get; set; }
+        /// <summary>
+        /// 保险项目
+        /// </summary>
+        public string car_project { get; set; }
+        /// <summary>
+        /// 截至保险日期
+        /// </summary>
+        public DateTime car_Insurance { get; set; }
+        /// <summary>
+        /// 保险开始日期
+        /// </summary>
+        public DateTime car_strat { get; set; }
+        /// <summary>
+        /// 保险结束日期
+        /// </summary>
+        public DateTime car_end { get; set; }
+    }
+}
