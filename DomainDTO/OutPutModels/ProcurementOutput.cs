2b341dd11f8ea1cb5299952d66f90f0fcb2bf4f3
Louts <username@email.com>
2022-03-02 20:27:02 +0800

    Su

--- DomainDTO/OutPutModels/ProcurementOutput.cs
+++ DomainDTO/OutPutModels/ProcurementOutput.cs
@@ -1,21 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DomainDTO.EFModels
+namespace DomainDTO.OutPutModels
 {
-    public class ProcurementDetails
+    public class ProcurementOutput
     {
-        /// <summary>
-        /// 日常用品采购详情表
-        /// </summary>
-        [Key]
         public int Goods_Id { get; set; }
-        public int TaskId { get; set; }
-
         /// <summary>
         /// 用品名称
         /// </summary>
---
+++ Project/Controllers/MeetingRoomController.cs
@@ -0,0 +1,53 @@
+﻿using DomainDTO.EFModels;
+using IServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Project.Controllers
+{
+    /// <summary>
+    /// 会议室控制器
+    /// </summary>
+    [ApiController]
+    public class MeetingRoomController : ControllerBase
+    {
+        private readonly IMeetingRoomServices meeting;
+        public MeetingRoomController(IMeetingRoomServices meeting)
+        {
+            this.meeting = meeting;
+        }
+        /// <summary>
+        /// 会议室新增
+        /// </summary>
+        /// <param name="roomnew"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("api/MeetingRoomNewAdd")]
+        public IActionResult MeetingRoomNewAdd(MeetingRoomNew roomnew)
+        {
+            return Ok(meeting.MeetingRoomNewAdd(roomnew));
+        }
+        /// <summary>
+        /// 会议室预定
+        /// </summary>
+        /// <param name="roomnew"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("api/ReservationMeetingRoomAdd")]
+        public IActionResult ReservationMeetingRoomAdd(ReservationMeetingRoom reserva)
+        {
+            return Ok(meeting.ReservationMeetingRoomAdd(reserva));
+        }
+        /// <summary>
+        /// 下拉框
+        /// </summary>
+        /// <param name="reserva"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("api/GetMeetingRoom")]
+        public IActionResult GetMeetingRoom(int id)
+        {
+            return Ok(meeting.GetMeetingRoom(id));
+        }
+    }
+}
